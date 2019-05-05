fbbc6d93c458d544f0df3ce5e16928d2d246a649
Matrix <33636578+Matrix07ksa@users.noreply.github.com>
2019-05-05 11:14:28 +0300

    Create Dollar.c

---
+++ Dollar.c
@@ -0,0 +1,20 @@
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+void convertrd(float dollar);
+int main()
+{
+    float dollar;
+    printf(" Write You To Dollar:");
+    scanf("%f",&dollar);
+    convertrd(dollar);
+    return 0;
+}
+void convertrd(float dollar)
+{
+    float saudi = dollar * 3.75;
+    printf("\n%.2f Dollar = %2.f Saudi Reyal",dollar,saudi);
+    return 0;
+}
